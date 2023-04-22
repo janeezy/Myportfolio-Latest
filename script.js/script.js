dcbecd4574efcd498bf296628e132618ac6439f3
janeezy <janeezyofficial@gmail.com>
2023-04-22 11:21:47 +0100

    updated my recent projects and bio

---
+++ script.js/script.js
@@ -0,0 +1,41 @@
+'use script';
+// scroll to top functionality
+const scrollUp = document.querySelector('#scroll-up');
+
+scrollUp.addEventListener('click', () => {
+	window.scrollTo({
+		top: 0,
+		left: 0,
+		behavior: 'smooth',
+	});
+});
+
+// Nav hamburgerburger selections
+
+const burger = document.querySelector('#burger-menu');
+const ul = document.querySelector('nav ul');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click', () => {
+	ul.classList.toggle('show');
+});
+
+// Close hamburger menu when a link is clicked
+
+// Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll('.nav-link');
+
+navLink.forEach((link) =>
+	link.addEventListener('click', () => {
+		ul.classList.remove('show');
+	})
+);
+
+// * date = year.getFullYear('2023');
+
+const copyrightEl = document.getElementById('copyrightYear');
+copyrightEl.innerText = 'Copyright ' + new Date().getFullYear();
+
+//gets the button by ID from your HTML element
